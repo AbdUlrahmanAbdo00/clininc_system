0adbfa06ffc78ff25571b96a947c68e33455b02f
abdUlrahman <bbdw69243@gmail.com>
2025-05-25 15:02:32 +0300

    edit on shift table

--- database/migrations/2025_05_11_204859_create_shifts_table.php
+++ database/migrations/2025_05_11_204859_create_shifts_table.php
@@ -15,7 +15,10 @@ return new class extends Migration
             $table->id();
             $table->time('start_time');
             $table->time('end_time');
-            $table->string('shift_type')->unique(); 
+            $table->string('shift_type')->unique();
+            $table->time('start_break_time');
+            $table->time('end_break_time');
+
             $table->timestamps();
         });
     }
