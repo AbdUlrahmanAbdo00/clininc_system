035f3c0101b8b261331248ccbef94611a1e4addf
abdUlrahman <bbdw69243@gmail.com>
2025-06-24 23:08:04 +0300

    check

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -4,11 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Services\FirebaseService;
+use Illuminate\Support\Str;
 
 class NotificationController extends Controller
 {
     public function sendTestNotification(FirebaseService $firebase, Request $request)
-    {dd(env('FIREBASE_CREDENTIALS'));
+    {
+        $raw = env('FIREBASE_CREDENTIALS');
+
+// إزالة العلامات الزائدة إذا موجودة
+if (Str::startsWith($raw, '"') && Str::endsWith($raw, '"')) {
+    $raw = substr($raw, 1, -1); // إزالة أول وآخر " من النص
+    $raw = str_replace('\"', '"', $raw); // إعادة الاقتباسات الداخلية لوضعها الصحيح
+    $raw = str_replace('\\n', "\n", $raw); // تحويل \n لنهاية سطر حقيقية
+}
+$credentials = json_decode($raw, true);
+
+dd($credentials);
 
         $request->validate([
             'deviceToken' => 'required|string',
--- config/firebase.php
+++ config/firebase.php
@@ -1,5 +1,15 @@
 <?php 
+use Illuminate\Support\Str;
+
+$raw = env('FIREBASE_CREDENTIALS');
+
+// إزالة العلامات الزائدة إذا موجودة
+if (Str::startsWith($raw, '"') && Str::endsWith($raw, '"')) {
+    $raw = substr($raw, 1, -1); // إزالة أول وآخر " من النص
+    $raw = str_replace('\"', '"', $raw); // إعادة الاقتباسات الداخلية لوضعها الصحيح
+    $raw = str_replace('\\n', "\n", $raw); // تحويل \n لنهاية سطر حقيقية
+}
 
 return [
-'credentials' => json_decode(env('FIREBASE_CREDENTIALS'), true),
+'credentials' => json_decode($raw, true),
 ];
