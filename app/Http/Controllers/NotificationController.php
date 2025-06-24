564ba77d8d8a682e2576407c9f1d338e46a6f13a
abdUlrahman <bbdw69243@gmail.com>
2025-06-24 23:17:34 +0300

    check2

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -10,17 +10,7 @@ class NotificationController extends Controller
 {
     public function sendTestNotification(FirebaseService $firebase, Request $request)
     {
-        $raw = env('FIREBASE_CREDENTIALS');
-
-// إزالة العلامات الزائدة إذا موجودة
-if (Str::startsWith($raw, '"') && Str::endsWith($raw, '"')) {
-    $raw = substr($raw, 1, -1); // إزالة أول وآخر " من النص
-    $raw = str_replace('\"', '"', $raw); // إعادة الاقتباسات الداخلية لوضعها الصحيح
-    $raw = str_replace('\\n', "\n", $raw); // تحويل \n لنهاية سطر حقيقية
-}
-$credentials = json_decode($raw, true);
-
-dd($credentials);
+       
 
         $request->validate([
             'deviceToken' => 'required|string',
