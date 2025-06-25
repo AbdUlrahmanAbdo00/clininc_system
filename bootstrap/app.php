8478736138b407fc60eeea84f20b36f20a1b9519
abdUlrahman <bbdw69243@gmail.com>
2025-06-25 13:21:30 +0300

    solve problem for  middleware for doctor

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'patient' => \App\Http\Middleware\PatientMiddleware::class,
+            'doctor' => \App\Http\Middleware\DoctorMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
