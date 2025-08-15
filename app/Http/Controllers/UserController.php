e1066ab765969b8994d0b7c3035233a4ee134a67
abdUlrahman <bbdw69243@gmail.com>
2025-08-15 17:27:01 +0300

    request otp solve problem

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,154 +1,154 @@
-    <?php
+<?php
 
-    namespace App\Http\Controllers;
+namespace App\Http\Controllers;
 
-    use App\Models\Otp;
-    use App\Models\User;
-    use Illuminate\Http\Request;
-    use Illuminate\Support\Facades\Auth;
-    use Illuminate\Support\Facades\Validator;
-    use App\Traits\OtpTrait;
-    use Stichoza\GoogleTranslate\GoogleTranslate;
+use App\Models\Otp;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use App\Traits\OtpTrait;
+use Stichoza\GoogleTranslate\GoogleTranslate;
 
-    class UserController extends Controller
+class UserController extends Controller
+{
+    use OtpTrait;
+
+    public function requestOtp(Request $request)
     {
-        use OtpTrait;
+        $lan = $request->header('lan', 'en');
+        $translator = new GoogleTranslate($lan);
 
-        public function requestOtp(Request $request)
-        {
-            $lan = $request->header('lan', 'en');
-            $translator = new GoogleTranslate($lan);
+        $request->validate([
+            'number' => ['required', 'regex:/^(\+963|09)\d{8}$/'],
+            'role' => 'string|max:255',
+        ]);
 
-            $request->validate([
-                'number' => ['required', 'regex:/^(\+963|09)\d{8}$/'],
-                'role' => 'string|max:255',
-            ]);
+        $role = $request->role;
+        $check = true;
+        $num = 400;
+        $user = User::where('number', $request->number)->first();
 
-            $role = $request->role;
-            $check = true;
-            $num = 400;
-            $user = User::where('number', $request->number)->first();
-
-            if ($user) {
-                if ($role == "doctor" && !$user->hasRole('doctor')) {
-                    $check = false;
-                }
-            } else {
-                if ($role === "doctor") {
-                    $check = false;
-                }
+        if ($user) {
+            if ($role == "doctor" && !$user->hasRole('doctor')) {
+                $check = false;
             }
-
-            if ($check) {
-                $result = $this->sendOTP($request->number);
+        } else {
+            if ($role === "doctor") {
+                $check = false;
             }
-
-            if (isset($result['success']) && $result['success'] == true) {
-                $num = 200;
-            }
-
-            if (isset($result['data']) && is_array($result['data'])) {
-                $result['data']['check'] = $check;
-            }
-
-            return response()->json([
-                "success" => $result['success'] ?? false,
-                "message" => $translator->translate($result['message'] ?? 'Change the role to Patient role'),
-                "data" => $result['data'] ?? false,
-            ], $num);
         }
 
-        public function verif(Request $request)
-        {
-            $lan = $request->header('lan', 'en');
-            $translator = new GoogleTranslate($lan);
+        if ($check) {
+            $result = $this->sendOTP($request->number);
+        }
 
-            $validator = Validator::make($request->all(), [
-                'number' => 'required',
-                'otp' => 'required'
-            ]);
+        if (isset($result['success']) && $result['success'] == true) {
+            $num = 200;
+        }
 
-            $result = $this->verifyOTP($request->number, $request->otp);
+        if (isset($result['data']) && is_array($result['data'])) {
+            $result['data']['check'] = $check;
+        }
 
-            if (!$result['success']) {
-                return response()->json([
-                    'success' => false,
-                    'message' => $translator->translate($result['message'] ?? 'Unauthorized'),
-                ], 400);
-            }
+        return response()->json([
+            "success" => $result['success'] ?? false,
+            "message" => $translator->translate($result['message'] ?? 'Change the role to Patient role'),
+            "data" => $result['data'] ?? false,
+        ], $num);
+    }
 
-            $filled_data = true;
-            $user = User::where('number', $request->number)->first();
+    public function verif(Request $request)
+    {
+        $lan = $request->header('lan', 'en');
+        $translator = new GoogleTranslate($lan);
 
-            $messag = 'Logged in successfully.';
+        $validator = Validator::make($request->all(), [
+            'number' => 'required',
+            'otp' => 'required'
+        ]);
 
-            if (!$user) {
-                $messag = 'Account created successfully. Welcome!';
-                $user = User::create([
-                    'number' => $request->number
-                ]);
-                $filled_data = false;
-            }
+        $result = $this->verifyOTP($request->number, $request->otp);
 
-            $requiredFields = [
-                'first_name', 'middle_name', 'last_name', 'number',
-                'mother_name', 'birth_day', 'national_number', 'gender'
-            ];
-
-            foreach ($requiredFields as $field) {
-                if (empty($user->$field)) {
-                    $filled_data = false;
-                    break;
-                }
-            }
-
-            $userOtp = Otp::where('phone', $request->number)->first();
-
-            if ($request->otp == $userOtp->otp) {
-                Auth::login($user);
-
-                $token = $user->createToken('clinic_sys')->plainTextToken;
-
-                return response()->json([
-                    'success' => true,
-                    'message' => $translator->translate($messag),
-                    'data' => [
-                        'token' => $token,
-                        'token_type' => 'bearer',
-                        'filled_data' => $filled_data,
-                    ]
-                ], 200);
-            }
-
+        if (!$result['success']) {
             return response()->json([
                 'success' => false,
-                'message' => $translator->translate('Unauthorized')
+                'message' => $translator->translate($result['message'] ?? 'Unauthorized'),
             ], 400);
         }
 
-        public function logout(Request $request)
-        {
-            $lan = $request->header('lan', 'en');
-            $translator = new GoogleTranslate($lan);
+        $filled_data = true;
+        $user = User::where('number', $request->number)->first();
 
-            $request->user()->currentAccessToken()->delete();
+        $messag = 'Logged in successfully.';
+
+        if (!$user) {
+            $messag = 'Account created successfully. Welcome!';
+            $user = User::create([
+                'number' => $request->number
+            ]);
+            $filled_data = false;
+        }
+
+        $requiredFields = [
+            'first_name', 'middle_name', 'last_name', 'number',
+            'mother_name', 'birth_day', 'national_number', 'gender'
+        ];
+
+        foreach ($requiredFields as $field) {
+            if (empty($user->$field)) {
+                $filled_data = false;
+                break;
+            }
+        }
+
+        $userOtp = Otp::where('phone', $request->number)->first();
+
+        if ($request->otp == $userOtp->otp) {
+            Auth::login($user);
+
+            $token = $user->createToken('clinic_sys')->plainTextToken;
 
             return response()->json([
                 'success' => true,
-                'message' => $translator->translate('Logged out successfully.')
+                'message' => $translator->translate($messag),
+                'data' => [
+                    'token' => $token,
+                    'token_type' => 'bearer',
+                    'filled_data' => $filled_data,
+                ]
             ], 200);
         }
 
-        public function logoutFromAll(Request $request)
-        {
-            $lan = $request->header('lan', 'en');
-            $translator = new GoogleTranslate($lan);
-
-            $request->user()->tokens()->delete();
-
-            return response()->json([
-                'success' => true,
-                'message' => $translator->translate('Logged out successfully.')
-            ], 200);
-        }
+        return response()->json([
+            'success' => false,
+            'message' => $translator->translate('Unauthorized')
+        ], 400);
     }
+
+    public function logout(Request $request)
+    {
+        $lan = $request->header('lan', 'en');
+        $translator = new GoogleTranslate($lan);
+
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'success' => true,
+            'message' => $translator->translate('Logged out successfully.')
+        ], 200);
+    }
+
+    public function logoutFromAll(Request $request)
+    {
+        $lan = $request->header('lan', 'en');
+        $translator = new GoogleTranslate($lan);
+
+        $request->user()->tokens()->delete();
+
+        return response()->json([
+            'success' => true,
+            'message' => $translator->translate('Logged out successfully.')
+        ], 200);
+    }
+}
